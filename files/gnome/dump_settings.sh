d06839143a58a307178c8b07b515fa2ba7873a3e
Diego Aldarondo <diegoaldarondo@gmail.com>
2023-11-19 11:29:40 -0500

    add interface settings

--- files/gnome/dump_settings.sh
+++ files/gnome/dump_settings.sh
@@ -1,2 +1,3 @@
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > media-keys-shortcuts.dconf
 dconf dump /org/gnome/terminal/ > gnome_terminal_settings.dconf
+dconf dump /org/gnome/desktop/interface/ > gnome_interface_settings.dconf
