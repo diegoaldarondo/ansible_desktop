9f09a76f736eb7a0a773aea64eabb00bfb873c40
Diego Aldarondo <diegoaldarondo@gmail.com>
2023-12-27 16:28:37 -0500

    feat: Enhance script functionality and naming conventions
    - Renamed all function names to start with 'auto_' for better clarity and consistency.
    - Added a new function 'auto_code_review' to perform code review tasks.
    - Updated the 'autoedit' function to include the new 'Code Review' option in the selection menu.

--- files/scripts/edit.sh
+++ files/scripts/edit.sh
@@ -5,7 +5,18 @@ declare _MODEL=gpt-4-1106-preview
 declare _TEMPERATURE=.7
 declare _GPT_PARAMS="--model=$_MODEL --temperature=$_TEMPERATURE"
 
-format() {
+auto_code_review() {
+    local file="$1"
+
+    if [ ! -f "$file" ]; then
+        echo "Error: File does not exist."
+        return
+    fi
+
+    sgpt --role=code_review $_GPT_PARAMS <"$file" | code -
+}
+
+auto_format() {
     local filetype
     filetype=$(file --mime-type -b "$1")
 
@@ -18,15 +29,15 @@ format() {
     fi
 }
 
-add_docstring() {
+auto_docstring() {
     sgpt --role=docstring $_GPT_PARAMS <"$1" | code -d "$1" -
 }
 
-add_type_hints() {
+auto_type_hints() {
     sgpt --role=typehint $_GPT_PARAMS <"$1" | code -d "$1" -
 }
 
-lint() {
+auto_lint() {
     local filetype
     filetype=$(file --mime-type -b "$1")
 
@@ -37,18 +48,18 @@ lint() {
     fi
 }
 
-improve_code() {
+auto_improve_code() {
     sgpt --role=improve_code $_GPT_PARAMS <"$1" | code -d "$1" -
 }
 
-write_unit_tests() {
+auto_write_unit_tests() {
     {
         echo "Filename: $1"
         cat "$1"
     } | sgpt --role=UnitTestWriter $_GPT_PARAMS | code -
 }
 
-develop() {
+auto_develop() {
     local description
     description="$2"
 
@@ -60,7 +71,7 @@ develop() {
     sgpt --role=developer $_GPT_PARAMS "$description" <"$1" | code -d - "$1"
 }
 
-general_gpt() {
+auto_gpt() {
     local prompt
     prompt="$2"
 
@@ -94,20 +105,21 @@ autoedit() {
 
     [ -z "$file" ] && echo "No file selected." && return
 
-    local options=("Format" "Docstrings" "Develop" "Type Hints" "Lint" "Improve Code" "Write Unit Tests" "General" "Quit")
+    local options=("Format" "Docstrings" "Develop" "Type Hints" "Lint" "Improve Code" "Write Unit Tests" "General" "Code Review" "Quit")
     while true; do
         local opt
         opt=$(printf '%s\n' "${options[@]}" | fzf --prompt="Select an action: " --layout=reverse --border=rounded --margin=0 --padding=1 --color=dark --pointer="=>")
 
         case $opt in
-            "Format") format "$file" ;;
-            "Docstrings") add_docstring "$file" ;;
-            "Type Hints") add_type_hints "$file" ;;
-            "Lint") lint "$file" ;;
-            "Improve Code") improve_code "$file" ;;
-            "Write Unit Tests") write_unit_tests "$file" ;;
-            "Develop") develop "$file" ;;
-            "General") general_gpt "$file" ;;
+            "Format") auto_format "$file" ;;
+            "Docstrings") auto_docstring "$file" ;;
+            "Type Hints") auto_type_hints "$file" ;;
+            "Lint") auto_lint "$file" ;;
+            "Improve Code") auto_improve_code "$file" ;;
+            "Write Unit Tests") auto_write_unit_tests "$file" ;;
+            "Develop") auto_develop "$file" ;;
+            "General") auto_gpt "$file" ;;
+            "Code Review") auto_code_review "$file" ;;
             "Quit") break ;;
             *) break ;;
         esac
