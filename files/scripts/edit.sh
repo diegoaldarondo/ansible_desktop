2ec0bf3b7b0185d05be8f683c21fdafc5392c674
Diego Aldarondo <diegoaldarondo@gmail.com>
2023-12-27 15:51:59 -0500

    feat: Refactor bash aliases and move code editing functions to separate script
    - Removed code editing functions from .bash_aliases and moved them to a new script file, edit.sh.
    - Added sourcing of the new script file in .bash_aliases.
    - The new script includes functions for formatting, adding docstrings, type hints, linting, improving code, writing unit tests, developing, and general GPT.
    - Also includes a function to check if all required command line tools are installed.
    - The autoedit function in the new script allows for automatic editing of a provided file with a selection of actions.

---
+++ files/scripts/edit.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+# edit - A CLI tool for modifying code with LLMs.
+
+declare _MODEL=gpt-4-1106-preview
+declare _TEMPERATURE=.7
+declare _GPT_PARAMS="--model=$_MODEL --temperature=$_TEMPERATURE"
+
+format() {
+    """
+    Format the provided file according to its file type.
+
+    Args:
+        $1 (str): Path to the file to be formatted.
+    """
+    local filetype
+    filetype=$(file --mime-type -b "$1")
+
+    if [[ $filetype == "text/x-python" ]]; then
+        black "$1"
+        isort "$1"
+        autoflake --remove-all-unused-imports --in-place "$1"
+    elif [[ $filetype == "text/x-shellscript" ]]; then
+        shfmt -i 4 -ci -s -w "$1"
+    fi
+}
+
+add_docstring() {
+    """
+    Add docstrings to the provided file.
+
+    Args:
+        $1 (str): Path to the file.
+    """
+    sgpt --role=docstring $_GPT_PARAMS <"$1" | code -d "$1" -
+}
+
+add_type_hints() {
+    """
+    Add type hints to the provided python file.
+
+    Args:
+        $1 (str): Path to the python file.
+    """
+    sgpt --role=typehint $_GPT_PARAMS <"$1" | code -d "$1" -
+}
+
+lint() {
+    """
+    Lint the provided file according to its file type.
+
+    Args:
+        $1 (str): Path to the file to be linted.
+    """
+    local filetype
+    filetype=$(file --mime-type -b "$1")
+
+    if [[ $filetype == "text/x-python" ]]; then
+        cat "$1" <(pylint "$1") | sgpt --role=pylint $_GPT_PARAMS | code -d "$1" -
+    elif [[ $filetype == "text/x-shellscript" ]]; then
+        cat "$1" <(shellcheck -f gcc "$1") | sgpt --role=shellcheck $_GPT_PARAMS | code -d "$1" -
+    fi
+}
+
+improve_code() {
+    """
+    Improve the provided code.
+
+    Args:
+        $1 (str): Path to the file.
+    """
+    sgpt --role=improve_code $_GPT_PARAMS <"$1" | code -d "$1" -
+}
+
+write_unit_tests() {
+    """
+    Write unit tests for the provided file.
+
+    Args:
+        $1 (str): Path to the file.
+    """
+    {
+        echo "Filename: $1"
+        cat "$1"
+    } | sgpt --role=UnitTestWriter $_GPT_PARAMS | code -
+}
+
+develop() {
+    """
+    Develop file based on provided description.
+
+    Args:
+        $1 (str): Path to the file.
+        $2 (str): Description of the change.
+    """
+    local description
+    description="$2"
+
+    if [ -z "$description" ]; then
+        read -r -p "Enter a description of the change: " description
+        [ -z "$description" ] && echo "No description provided." && return
+    fi
+
+    sgpt --role=developer $_GPT_PARAMS "$description" <"$1" | code -d - "$1"
+}
+
+general_gpt() {
+    """
+    Run GPT on the provided file with a prompt.
+
+    Args:
+        $1 (str): Path to the file.
+        $2 (str): The prompt.
+    """
+    local prompt
+    prompt="$2"
+
+    if [ -z "$prompt" ]; then
+        read -r -p "Enter prompt: " prompt
+        [ -z "$prompt" ] && echo "No prompt provided." && return
+    fi
+
+    sgpt $_GPT_PARAMS "$prompt" <"$1" | code -
+}
+
+check_installation() {
+    """
+    Check if all of the required command line tools are installed.
+    """
+    # Check if all of the required command line tools are installed
+    local required_tools=("black" "isort" "autoflake" "sgpt" "code" "fdfind" "fzf" "bat" "pylint")
+
+    for tool in "${required_tools[@]}"; do
+        if ! command -v "$tool" &>/dev/null; then
+            echo "Error: $tool is not installed." >&2
+            exit 1
+        fi
+    done
+}
+
+autoedit() {
+    """
+    A function to automatically edit the provided file.
+
+    Args:
+        $1 (str): Path to the file to be edited.
+    """
+    check_installation
+    local file="$1"
+
+    if [ ! -f "$file" ]; then
+        file=$(fdfind -H --color=always -t f --follow . ~ | fzf --ansi --preview 'bat --color=always --theme="OneHalfDark" {}' --preview-window=right:50%,border-rounded --layout=reverse --border=rounded --margin=0 --padding=1 --color=dark --prompt="Select a file: " --pointer="=>")
+    fi
+
+    [ -z "$file" ] && echo "No file selected." && return
+
+    local options=("Format" "Docstrings" "Develop" "Type Hints" "Lint" "Improve Code" "Write Unit Tests" "General" "Quit")
+    while true; do
+        local opt
+        opt=$(printf '%s\n' "${options[@]}" | fzf --prompt="Select an action: " --layout=reverse --border=rounded --margin=0 --padding=1 --color=dark --pointer="=>")
+
+        case $opt in
+            "Format") format "$file" ;;
+            "Docstrings") add_docstring "$file" ;;
+            "Type Hints") add_type_hints "$file" ;;
+            "Lint") lint "$file" ;;
+            "Improve Code") improve_code "$file" ;;
+            "Write Unit Tests") write_unit_tests "$file" ;;
+            "Develop") develop "$file" ;;
+            "General") general_gpt "$file" ;;
+            "Quit") break ;;
+            *) break ;;
+        esac
+    done
+}
