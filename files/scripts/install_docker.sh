4494ee8a2f8c2d5f61ca45ab2c8f3768dd5c44b5
Diego Aldarondo <diegoaldarondo@gmail.com>
2024-01-11 20:08:38 -0500

    ``` Fix incorrect option order and add missing '-y' flag in install_docker.sh
    The install_docker.sh script has been updated to correct the order of options for the apt-get install command and to include the missing '-y' flag, ensuring unattended installation of packages without user interaction.
    ```

--- files/scripts/install_docker.sh
+++ files/scripts/install_docker.sh
@@ -1,6 +1,6 @@
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg -Y
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -13,7 +13,7 @@ echo \
 sudo apt-get update
 
 # Install 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -Y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 
 # Test
 sudo docker run hello-world
